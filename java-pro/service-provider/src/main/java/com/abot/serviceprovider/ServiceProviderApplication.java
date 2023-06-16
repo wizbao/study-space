bbe58d7f3174b2dc024e507fb1313b68d973f6fe
zhangweibao <xiangyangcao@7lk.com>
2023-06-16 18:11:17 +0800

    A  新增两个模块练习nacos服务注册发现

---
+++ java-pro/service-provider/src/main/java/com/abot/serviceprovider/ServiceProviderApplication.java
@@ -0,0 +1,27 @@
+package com.abot.serviceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient //开启服务注册发现功能
+public class ServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderApplication.class, args);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+
+}
